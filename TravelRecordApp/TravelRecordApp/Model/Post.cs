2336b673f7ea94c705aeb90cb1b8cc7502754b4b
Paras Rawat <parasrawat67@gmail.com>
2021-10-14 20:30:40 +0530

    SQLite Atributes--Resolving Xamarin.Forms Version Issues

--- TravelRecordApp/TravelRecordApp/Model/Post.cs
+++ TravelRecordApp/TravelRecordApp/Model/Post.cs
@@ -1,8 +1,13 @@
-﻿namespace TravelRecordApp.Model
+﻿using SQLite;
+
+namespace TravelRecordApp.Model
 {
     public class Post
     {
+        [PrimaryKey,AutoIncrement]
         public int Id { get; set; }
+
+        [MaxLength(250)]        
         public string Experience { get; set; }
     }
 }
