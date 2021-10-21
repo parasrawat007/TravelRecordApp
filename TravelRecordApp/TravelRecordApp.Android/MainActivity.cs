37ccc4f88bbe97def84783708522b86872c4b4b3
Paras Rawat <parasrawat67@gmail.com>
2021-10-21 22:56:33 +0530

    Displaying the User's Location Dowloading Xamarin Essentials For taking permissions

--- TravelRecordApp/TravelRecordApp.Android/MainActivity.cs
+++ TravelRecordApp/TravelRecordApp.Android/MainActivity.cs
@@ -19,12 +19,18 @@ namespace TravelRecordApp.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             Xamarin.FormsMaps.Init(this,savedInstanceState);
             string DbName = "Travel_Db.sqlite";
             string FolderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             string FullPath = Path.Combine(FolderPath, DbName);
             LoadApplication(new App(FullPath));
         }
-      
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
--- TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace TravelRecordApp
 {
@@ -16,5 +17,38 @@ namespace TravelRecordApp
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetLocation();
+        }
+
+        private async void GetLocation()
+        {
+           var status = await CheckAndRequestPermission();
+            if (status == PermissionStatus.Granted)
+            {
+                var location = await Geolocation.GetLocationAsync();
+                MapLocation.IsShowingUser = true;    
+            }
+        }
+
+        private async Task<PermissionStatus> CheckAndRequestPermission()
+        {
+            var status =await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (status == PermissionStatus.Granted)
+            {
+                MapLocation.IsShowingUser = true;
+                return status;
+            }
+            else if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                //propm the user to turn on the permission in settings
+                return status;
+            }
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            return status;
+        }
+    }
 }
