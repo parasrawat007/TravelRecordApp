557defb8473bd355206a01e7d365551b3163b383
Paras Rawat <parasrawat67@gmail.com>
2021-10-22 23:30:41 +0530

    Creating URL for REST Request

---
+++ TravelRecordApp/TravelRecordApp/Model/Venue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelRecordApp.Helpers;
+
+namespace TravelRecordApp.Model
+{
+    public class Venue
+    {
+
+        public static string GenerateUrl(double latitude,double longitude)
+        {
+            return string.Format(Constants.VenueSearch, latitude, longitude, Constants.ClientId, Constants.ClientSecret,DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
