86cc3fc3f793284eeb985bc1d32a9e49c46ab790
Paras Rawat <parasrawat67@gmail.com>
2021-10-29 22:52:49 +0530

    Profile Page

--- TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +17,14 @@ namespace TravelRecordApp
 		{
 			InitializeComponent ();
 		}
-	}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            using (var con = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var PostTable = con.Table<Post>().ToList();
+                LabelPostCount.Text = PostTable.Count.ToString();
+            }
+        }
+    }
 }
