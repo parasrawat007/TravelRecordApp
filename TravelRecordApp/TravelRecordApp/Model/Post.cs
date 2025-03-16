6fd44781b0230e38e466c1819ebbd92d40e6f1ad
parasrawat007 <parasrawat67@gmail.com>
2025-03-16 23:21:45 +0530

    The Model of a Database Table

---
+++ TravelRecordApp/TravelRecordApp/Model/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp.Model
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Experience { get; set; }
+    }
+}
