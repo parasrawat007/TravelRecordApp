4fdf64fa4fd40b0ae809996a43769fb9674b984f
parasrawat007 <parasrawat67@gmail.com>
2025-03-16 16:51:50 +0530

    The Login Logic

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -17,7 +17,15 @@ namespace TravelRecordApp
 
         private void LoginButton_Clicked(object sender, EventArgs e)
         {
-
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);  
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+               
+            }
+            else
+            {
+            }
         }
     }
 }
