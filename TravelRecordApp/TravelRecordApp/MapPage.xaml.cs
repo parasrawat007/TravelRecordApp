71516a6ec42041684c0aa50f3ab797869b213cb2
parasrawat007 <parasrawat67@gmail.com>
2025-03-22 22:26:50 +0530

    Displaying the User's Location

--- TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +16,32 @@ namespace TravelRecordApp
 		{
 			InitializeComponent ();
 		}
-	}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+			GetLocation();
+        }
+
+        private async void GetLocation()
+		{ 
+			var staus= await CheckAndRequestLocationPermission();
+			if (staus == PermissionStatus.Granted)
+			{
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                locationsMap.IsShowingUser = true;
+			}
+        }
+
+        private async Task<PermissionStatus> CheckAndRequestLocationPermission()
+        {
+			var status=await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+			if (status == PermissionStatus.Granted)
+				return status;
+			else if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+				await DisplayAlert("Need permission",  "We will have to access your location so please allow access from settings",  "Ok",  "Cancel");
+
+            status= await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+			return status;
+        }
+    }
 }
