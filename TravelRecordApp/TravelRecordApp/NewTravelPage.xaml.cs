67d1d2b7babf1a4a665cc7f60bb0975b8b17271a
parasrawat007 <parasrawat67@gmail.com>
2025-03-23 17:32:26 +0530

    61. Creating URL for REST Request

--- TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using SQLite;
+﻿using Plugin.Geolocator;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelRecordApp.Logic;
 using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -18,12 +20,20 @@ namespace TravelRecordApp
 			InitializeComponent ();
 		}
 
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			var location = CrossGeolocator.Current;
+			var position =await location.GetPositionAsync();
+
+			var venues = VenueLogic.GetVenues(position.Latitude, position.Longitude);
+		}
 		private void Save_Clicked(object sender, EventArgs e)
 		{
 			Post post = new Post()
-            {
-                Experience = experienceEntry.Text
-            };
+			{
+				Experience = experienceEntry.Text
+			};
 			using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
 			{
 				conn.CreateTable<Post>();
@@ -38,6 +48,6 @@ namespace TravelRecordApp
 					DisplayAlert("Failure", "Experience failed to be inserted", "Ok");
 				}
 			}
-        }
+		}
 	}
 }
