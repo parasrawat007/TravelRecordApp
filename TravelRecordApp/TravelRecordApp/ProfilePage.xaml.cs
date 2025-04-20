d4929cc47ea197604e8f756486cc7399153816ae
parasrawat007 <parasrawat67@gmail.com>
2025-04-20 23:24:16 +0530

    Linq Where - Filtering a list

--- TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -17,15 +17,21 @@ namespace TravelRecordApp
 		{
 			InitializeComponent ();
 		}
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
 			using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
 			{
 				var postTable = conn.Table<Post>().ToList();
 				var categories =postTable.OrderBy(x=>x.CategoryId).Select(x=>x.CategoryName).Distinct().ToList();
+				Dictionary<string, int> categoryCount = new Dictionary<string, int>();
+                foreach (var category in categories)
+				{
+					var count = postTable.Count(x => x.CategoryName == category);
+                    categoryCount.Add(category, count);	
+                }
                 postCountLabel.Text = postTable.Count.ToString();	
-            }
-        }
-    }
+			}
+		}
+	}
 }
